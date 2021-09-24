c5995395eaffbf801a8dbf842cd9b16259c358f4
sandipan898 <sandipan.das898@gmail.com>
2021-09-25 03:16:24 +0530

    adjlist graph

--- src/pkg/nonlineasds/Graph.java
+++ src/pkg/nonlineasds/Graph.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 
 public class Graph {
     protected int adjMatrix[][];
+    protected ArrayList<ArrayList<Integer>> adjList;
     int n = 0;
+
     public Graph(int node) {
         n = node;
     }
@@ -21,13 +23,49 @@ public class Graph {
             adjMatrix[u][v] = 1;
             adjMatrix[v][u] = 1;
         }
+        scan.close();
         return adjMatrix;
     }
+
     public void showAdjMatrix() {
         for(int i=0; i<=n; i++) {
-//            System.out.print(i + " - ");
             for(int j=0; j<=n; j++) {
                 System.out.print(adjMatrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void makeAdjList() {
+        adjList = new ArrayList<ArrayList<Integer>>();
+        Scanner scan = new Scanner(System.in);
+
+        for (int i=0; i<=n; i++) {
+            adjList.add(new ArrayList<Integer>());
+        }
+        System.out.print("Enter number of Edges: ");
+        int e = scan.nextInt();
+        for (int i=0; i<e; i++) {
+            System.out.println("Enter the edge:");
+            int u = scan.nextInt();
+            int v = scan.nextInt();
+            
+            if (!adjList.get(u).contains(v) && !adjList.get(v).contains(u)) {
+                adjList.get(u).add(v);  
+                adjList.get(v).add(u);
+            } else {
+                System.out.println("Connection between " + u + " to " + v + " already exists!");
+                i--;
+            }
+        }
+        scan.close();
+    }
+    
+    public void showAdjList() {
+        for (int i=0; i<adjList.size(); i++) {
+            System.out.print("V " + i + ": ");
+            for (int j=0; j<adjList.get(i).size(); j++) {
+                System.out.print("--> " + adjList.get(i).get(j));
             }
             System.out.println();
         }
--- src/pkg/testclass/TestGraph.java
+++ src/pkg/testclass/TestGraph.java
@@ -3,8 +3,10 @@ import  pkg.nonlineasds.Graph;
 
 public class TestGraph {
     public static void main(String[] args) {
-        Graph G = new Graph(3);
-        G.makeAdjMatrix();
-        G.showAdjMatrix();
+        Graph G = new Graph(5);
+        // G.makeAdjMatrix();
+        // G.showAdjMatrix();
+        G.makeAdjList();
+        G.showAdjList();
     }
 }
